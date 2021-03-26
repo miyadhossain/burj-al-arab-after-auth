bc6558b0a443ba6a1385fce6f0ed41e6b7fcb739
miyadhossain <miyad.mh@gmail.com>
2021-03-26 14:52:51 +0600

    redirect update

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -25,7 +25,7 @@ const Login = () => {
         const signedInUser = { name: displayName, email };
         setLoggedInUser(signedInUser);
         storeAuthToken();
-        history.replace(from);
+
         // ...
       })
       .catch(function (error) {
@@ -41,6 +41,7 @@ const Login = () => {
       .then(function (idToken) {
         // console.log(idToken);
         sessionStorage.setItem("token", idToken);
+        history.replace(from);
       })
       .catch(function (error) {
         // Handle error
