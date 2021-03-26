eb36c322dc07de03d15a407a253f42a2688e1494
miyadhossain <miyad.mh@gmail.com>
2021-03-26 13:00:44 +0600

    connect to database done

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,10 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAsihSADFnF_FoZ6je8p1W9lilyqX7cHWQ",
-    authDomain: "burj-al-arab.firebaseapp.com",
-    databaseURL: "https://burj-al-arab.firebaseio.com",
-    projectId: "burj-al-arab",
-    storageBucket: "burj-al-arab.appspot.com",
-    messagingSenderId: "244522202387",
-    appId: "1:244522202387:web:f3801d0588387140e1d6e6"
-  };
-  export default firebaseConfig;
+  apiKey: "AIzaSyD-Dyy0vCYZms3M78u5DAOYJ1dWfqzvqZ4",
+  authDomain: "burjj-al-aarab.firebaseapp.com",
+  projectId: "burjj-al-aarab",
+  storageBucket: "burjj-al-aarab.appspot.com",
+  messagingSenderId: "251525802976",
+  appId: "1:251525802976:web:b87782b7e92d10f8a01985",
+};
+export default firebaseConfig;
